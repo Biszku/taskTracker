8c8b284d262c7e1a8836afb6b6ea5afc8424e8c7
Biszku <daniel.bivhuniak@gmail.com>
2024-08-17 16:36:23 +0200

    added initialization

--- src/main/java/com/biszku/taskTracker/MainClass.java
+++ src/main/java/com/biszku/taskTracker/MainClass.java
@@ -1,4 +1,10 @@
 package main.java.com.biszku.taskTracker;
 
 public class MainClass {
+
+    public static void main(String[] args) {
+
+        TasksTracker TasksTracker = new TasksTracker();
+        TasksTracker.start();
+    }
 }
