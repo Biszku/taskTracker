a2e6996ef1bd2aeef23d1f59ea3cb41d7e6694d1
Biszku <daniel.bivhuniak@gmail.com>
2024-08-17 21:07:00 +0200

    refactoring and implemented error handling

--- src/main/java/com/biszku/taskTracker/TasksTracker.java
+++ src/main/java/com/biszku/taskTracker/TasksTracker.java
@@ -6,41 +6,19 @@ public class TasksTracker {
 
     private static final Scanner scanner = new Scanner(System.in);
     private static int idCounter = 0;
-    private HashMap<Integer,Task> tasks;
+    private final HashMap<Integer,Task> tasks = new HashMap<>();
 
-    public TasksTracker() {
-        this.tasks = new HashMap<>();
+    public static void run() {
+        TasksTracker tasksTracker = new TasksTracker();
+        tasksTracker.menuController();
     }
 
-    public void start() {
+    private void menuController() {
+
         while (true) {
             Stack<String> commands = getCommands();
-            String operationType = commands.pop();
-            switch (operationType) {
-                case "ADD":
-                    addTask(commands.pop());
-                    break;
-                case "UPDATE":
-                    tasks.get(Integer.parseInt(commands.pop())).update(commands.pop());
-                    break;
-                case "DELETE":
-                    tasks.remove(Integer.parseInt(commands.pop()));
-                    break;
-                case "MARK-IN-PROGRESS":
-                    tasks.get(Integer.parseInt(commands.pop())).markAsInProgress();
-                    break;
-                case "MARK-DONE":
-                    tasks.get(Integer.parseInt(commands.pop())).markAsDone();
-                    break;
-                case "LIST":
-                    printTasks(commands);
-                    break;
-                case "EXIT":
-                    System.exit(0);
-                    return;
-                default:
-                    System.out.println("Unknown command");
-            }
+            boolean executed = executeCommand(commands);
+            if (!executed) break;
         }
     }
 
@@ -48,17 +26,67 @@ public class TasksTracker {
 
         Stack<String> commandsStack = new Stack<>();
         String[] commandStructure = scanner.nextLine().toUpperCase().split(" ");
-
         for (int i = commandStructure.length - 1; i >= 0; i--) {
             commandsStack.push(commandStructure[i]);
         }
-
         return commandsStack;
     }
 
-    private void addTask(String description) {
+    private boolean executeCommand(Stack<String> commands) {
+
+        String operationType = commands.pop();
+        switch (operationType) {
+            case "ADD":
+                addTask(commands);
+                break;
+            case "UPDATE":
+                updateTask(commands);
+                break;
+            case "DELETE":
+                deleteTask(commands);
+                break;
+            case "MARK-IN-PROGRESS":
+                markAsInProgress(commands);
+                break;
+            case "MARK-DONE":
+                markAsDone(commands);
+                break;
+            case "LIST":
+                printTasks(commands);
+                break;
+            case "EXIT":
+                return false;
+            default:
+                System.out.println("Unknown command");
+        }
+        return true;
+    }
+
+    private void addTask(Stack<String> commands) {
+        String description = commands.pop();
         Task newTask = new Task(description);
         tasks.put(++idCounter, newTask);
+    }
+
+    private void updateTask(Stack<String> commands) {
+        int taskId = Integer.parseInt(commands.pop());
+        String description = commands.pop();
+        tasks.get(taskId).update(description);
+    }
+
+    private void deleteTask(Stack<String> commands) {
+        int taskId = Integer.parseInt(commands.pop());
+        tasks.remove(taskId);
+    }
+
+    private void markAsInProgress(Stack<String> commands) {
+        int taskId = Integer.parseInt(commands.pop());
+        tasks.get(taskId).markAsInProgress();
+    }
+
+    private void markAsDone(Stack<String> commands) {
+        int taskId = Integer.parseInt(commands.pop());
+        tasks.get(taskId).markAsDone();
     }
 
     private void printTasks(Stack<String> commands) {
@@ -74,8 +102,9 @@ public class TasksTracker {
             for (Map.Entry<Integer, Task> entry : tasks.entrySet()) {
                 System.out.println("id = " + entry.getKey() + " " + entry.getValue());
             }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Unknown status");
         }
-
     }
 
     public Status getTaskStatus(Map.Entry<Integer, Task> task) {
