03dbd256db5985e805258b452c36e4905560aac3
Biszku <daniel.bivhuniak@gmail.com>
2024-08-17 17:22:44 +0200

    implemented common Object

--- src/main/java/com/biszku/taskTracker/Task.java
+++ src/main/java/com/biszku/taskTracker/Task.java
@@ -3,26 +3,42 @@ package main.java.com.biszku.taskTracker;
 import java.time.LocalDate;
 
 public class Task {
-    private int id;
     private String description;
     private Status status;
-    private LocalDate createdAt;
+    private final LocalDate createdAt;
     private LocalDate updatedAt;
 
-    public Task(int id) {
-        this(id, "");
-    }
-
-    public Task(int id, String description) {
-        this.id = id;
+    public Task(String description) {
         this.description = description;
         this.status = Status.TODO;
         this.createdAt = LocalDate.now();
         this.updatedAt = LocalDate.now();
     }
 
-    public void update(String description, Status status) {
+    public Status getStatus() {
+        return status;
+    }
+
+    public void update(String description) {
         this.description = description;
         this.updatedAt = LocalDate.now();
+    }
+
+    public void markAsInProgress() {
+        this.status = Status.IN_PROGRESS;
+        this.updatedAt = LocalDate.now();
+    }
+
+    public void markAsDone() {
+        this.status = Status.DONE;
+        this.updatedAt = LocalDate.now();
+    }
+
+    @Override
+    public String toString() {
+        return ("description = %s " +
+                "status = %s " +
+                "created = %s " +
+                "updated = %s").formatted(description, status, createdAt, updatedAt);
     }
 }
