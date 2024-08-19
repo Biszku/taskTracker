f8457221c91b4a4b8347189fdba8e56366d2cdf4
Biszku <daniel.bivhuniak@gmail.com>
2024-08-19 21:04:56 +0200

    implemented String representation

--- src/main/java/com/biszku/taskTracker/Status.java
+++ src/main/java/com/biszku/taskTracker/Status.java
@@ -1,5 +1,14 @@
 package main.java.com.biszku.taskTracker;
 
 public enum Status {
-    TODO, IN_PROGRESS, DONE
+    TODO, IN_PROGRESS, DONE;
+
+    @Override
+    public String toString() {
+        return switch (this) {
+            case TODO -> "TODO";
+            case IN_PROGRESS -> "IN_PROGRESS";
+            case DONE -> "DONE";
+        };
+    }
 }
