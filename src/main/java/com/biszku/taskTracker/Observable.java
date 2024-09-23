f4c1cf92c01c0ee523b384fee9e49e4443fb6766
Biszku <daniel.bivhuniak@gmail.com>
2024-09-23 16:41:30 +0200

    removed useless code

--- src/main/java/com/biszku/taskTracker/Observable.java
+++ src/main/java/com/biszku/taskTracker/Observable.java
@@ -3,6 +3,5 @@ package main.java.com.biszku.taskTracker;
 public interface Observable {
 
     void addObserver(Observer observer);
-    void removeObserver(Observer observer);
     void notifyObservers();
 }
